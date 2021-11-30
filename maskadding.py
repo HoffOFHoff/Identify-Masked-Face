7551dd92fab64c7e0c65970d50b62f5f3f8c47cb
Bo Chao <91092213+pkocattoss@users.noreply.github.com>
2021-11-30 16:32:42 -0500

    Update maskadding.py

--- maskadding.py
+++ maskadding.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 from PIL import Image, ImageFile
+import face_recognition
 
 IMAGE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'images')
 surgical = os.path.join(IMAGE_DIR, 'surgical.png')
