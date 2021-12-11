8c1a450982dd2e2b5296f9597cb8fe8ca7cc9725
Bo Chao <91092213+pkocattoss@users.noreply.github.com>
2021-12-11 15:52:23 -0500

    Update GUI.py
    update

--- GUI.py
+++ GUI.py
@@ -37,8 +37,8 @@ class Face_Register:
         self.label_faceinstorage = tk.Label(self.frame_right, text=str(self.existing_faces))
         self.label_fps = tk.Label(self.frame_right, text="")
         self.input_name = tk.Entry(self.frame_right)
-        self.input_name_char = ""
-        self.label.face = tk.Label(self.frame_right, text="Faces in frame belongs to:")
+        self.input_name_box = ""
+        self.label_face_in_frame = tk.Label(self.frame_right, text="Faces in frame belongs to:")
         self.log_total = tk.Label(self.frame_right)
         self.label_warning = tk.Label(self.frame_right)
 
@@ -79,5 +79,85 @@ class Face_Register:
         self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 180)
         self.cap.set(cv2.CAP_PROP_FPS, 25)
 
+    def create_face_folder(self):
+        # Create a folders for saving faces
+        self.existing_faces += 1
+        if self.input_name_box:
+            self.current_face_path = self.path_photos + \
+                                    "person_" + str(self.existing_faces) + "_" + \
+                                    self.input_name_box
+        else:
+            self.current_face_path = self.path_photos + \
+                                    "person_" + str(self.existing_faces)
+        os.makedirs(self.current_face_path)
+        self.log_total["text"] = "\"" + self.current_face_path + "/\" created!"
+        logging.info("\n%-40s %s", "Create folders:", self.current_face_path)
+
+        self.ss = 0  # Clear the screen shots
+        self.face_folder_created_flag = True  # Face folder already created
+
+    def GUI_get_input_name(self):
+        self.input_name_box = self.input_name.get()
+        self.create_face_folder()
+        self.label_faceinstorage['text'] = str(self.existing_faces)
+
+    def delete_previous_data(self):
+        pass
+
+    def save_current_photo(self):
+        pass
+
+    def GUI_info(self):
+        tk.Label(self.frame_right,
+                 text="Face register",
+                 font=self.font_title).grid(row=0, column=0, columnspan=3, sticky=tk.W, padx=2, pady=20)
+
+        tk.Label(self.frame_right,
+                 text="FPS: ").grid(row=1, column=0, columnspan=2, sticky=tk.W, padx=5, pady=2)
+        self.label_fps.grid(row=1, column=2, sticky=tk.W, padx=5, pady=2)
+
+        tk.Label(self.frame_right,
+                 text="Faces in storage: ").grid(row=2, column=0, columnspan=2, sticky=tk.W, padx=5, pady=2)
+        self.label_faceinstorage.grid(row=2, column=2, columnspan=3, sticky=tk.W, padx=5, pady=2)
+
+        tk.Label(self.frame_right,
+                 text="Faces in current frame: ").grid(row=3, column=0, columnspan=2, sticky=tk.W, padx=5, pady=2)
+        self.label_face_in_frame.grid(row=3, column=2, columnspan=3, sticky=tk.W, padx=5, pady=2)
+
+        self.label_warning.grid(row=4, column=0, columnspan=3, sticky=tk.W, padx=5, pady=2)
+
+        # Step 1: Clear old data
+        tk.Label(self.frame_right,
+                 font=self.font_step_title,
+                 text="Step 1: Clear previous photos").grid(row=5, column=0, columnspan=2, sticky=tk.W, padx=5, pady=20)
+        tk.Button(self.frame_right,
+                  text='Clear',
+                  command=self.delete_previous_data).grid(row=6, column=0, columnspan=3, sticky=tk.W, padx=5, pady=2)
+
+        # Step 2: Input name and create folders for photos
+        tk.Label(self.frame_right,
+                 font=self.font_step_title,
+                 text="Step 2: Input your name").grid(row=7, column=0, columnspan=2, sticky=tk.W, padx=5, pady=20)
+
+        tk.Label(self.frame_right, text="Name: ").grid(row=8, column=0, sticky=tk.W, padx=5, pady=0)
+        self.input_name.grid(row=8, column=1, sticky=tk.W, padx=0, pady=2)
+
+        tk.Button(self.frame_right,
+                  text='Input',
+                  command=self.delete_previous_data).grid(row=8, column=2, padx=5)
+
+        # Step 3: Save current face in frame
+        tk.Label(self.frame_right,
+                 font=self.font_step_title,
+                 text="Step 3: Save face image").grid(row=9, column=0, columnspan=2, sticky=tk.W, padx=5, pady=20)
+
+        tk.Button(self.frame_right,
+                  text='Save current face',
+                  command=self.save_current_photo).grid(row=10, column=0, columnspan=3, sticky=tk.W)
+
+        # Show log in GUI
+        self.log_total.grid(row=11, column=0, columnspan=20, sticky=tk.W, padx=5, pady=20)
+
+        self.frame_right.pack()
 
 
