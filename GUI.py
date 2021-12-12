f564adb0e5f506b782e683e38529898c4112d02d
Bo Chao <91092213+pkocattoss@users.noreply.github.com>
2021-12-12 14:56:35 -0500

    Update GUI.py
    update

--- GUI.py
+++ GUI.py
@@ -24,7 +24,7 @@ class Face_Register:
         self.window.title("Face Recognition")
 
         # The window size could be modified here.
-        self.window.size("1000*1000")
+        self.window.geometry("1300x550")
 
         # left side of the GUI
         self.frame_left = tk.Frame(self.window)
@@ -49,7 +49,7 @@ class Face_Register:
         self.font = cv2.FONT_HERSHEY_COMPLEX
 
         # Pathway setting for the GUI
-        self.path_photos = "data/faces_from_camera"
+        self.path_photos = "data/faces_from_camera/"
         self.current_face_path = ""
 
         # Current frame and face ROI position
@@ -96,16 +96,47 @@ class Face_Register:
         self.ss = 0  # Clear the screen shots
         self.face_folder_created_flag = True  # Face folder already created
 
-    def GUI_get_input_name(self):
+    def GUI_input_name(self):
         self.input_name_box = self.input_name.get()
         self.create_face_folder()
         self.label_faceinstorage['text'] = str(self.existing_faces)
 
     def delete_previous_data(self):
-        pass
+        # Clears up previous data saved in the folder
+        folders_path = os.listdir(self.path_photos)
+        for i in range(len(folders_path)):
+            shutil.rmtree(self.path_photos + folders_path[i])
+        if os.path.isfile("data/features_all.csv"):
+            os.remove("data/features_all.csv")
+        self.label_faceinstorage['text'] = "0"
+        self.existing_faces_cnt = 0
+        self.log_total["text"] = "Photos and their`.csv` files are removed!"
 
     def save_current_photo(self):
-        pass
+        if self.face_folder_created_flag:
+            if self.current_frame_faces_cnt == 1:
+                if not self.out_of_range_flag:
+                    self.ss += 1
+                    # Create blank image according to the size of face detected
+                    self.face_ROI_image = np.zeros((int(self.face_ROI_height * 2), self.face_ROI_width * 2, 3),
+                                                   np.uint8)
+                    for ii in range(self.face_ROI_height * 2):
+                        for jj in range(self.face_ROI_width * 2):
+                            self.face_ROI_image[ii][jj] = self.current_frame[self.face_ROI_height_start - self.hh + ii][
+                                self.face_ROI_width_start - self.ww + jj]
+                    self.log_total["text"] = "\"" + self.current_face_path + "/img_face_" + str(
+                        self.ss) + ".png\"" + " saved!"
+                    self.face_ROI_image = cv2.cvtColor(self.face_ROI_image, cv2.COLOR_BGR2RGB)
+
+                    cv2.imwrite(self.current_face_path + "/img_face_" + str(self.ss) + ".png", self.face_ROI_image)
+                    logging.info("%-40s %s/img_face_%s.p", "Save into：",
+                                 str(self.current_face_path), str(self.ss) + ".png")
+                else:
+                    self.log_total["text"] = "Face out of range!"
+            else:
+                self.log_total["text"] = "No face in frame!"
+        else:
+            self.log_total["text"] = "Please register in step 2first!"
 
     def GUI_info(self):
         tk.Label(self.frame_right,
@@ -144,7 +175,7 @@ class Face_Register:
 
         tk.Button(self.frame_right,
                   text='Input',
-                  command=self.delete_previous_data).grid(row=8, column=2, padx=5)
+                  command=self.GUI_input_name).grid(row=8, column=2, padx=5)
 
         # Step 3: Save current face in frame
         tk.Label(self.frame_right,
@@ -160,4 +191,108 @@ class Face_Register:
 
         self.frame_right.pack()
 
+        # 新建保存人脸图像文件和数据 CSV 文件夹 / Mkdir for saving photos and csv
+    
+    def Folders(self):
+        # Create folders to save face images and csv
+        if os.path.isdir(self.path_photos):
+            pass
+        else:
+            os.mkdir(self.path_photos)
 
+    # If there are already people registered in the folder start from person_x+1
+    def check_existing_faces_cnt(self):
+        if os.listdir("data/data_faces_from_camera/"):
+            # Obtain the number of latest person
+            person_list = os.listdir("data/data_faces_from_camera/")
+            person_num_list = []
+            for person in person_list:
+                person_order = person.split('_')[1].split('_')[0]
+                person_num_list.append(person_order)
+            self.existing_faces_cnt = max(person_num_list)
+
+        # If no one has registered before, start from person_1
+        else:
+            self.existing_faces_cnt = 0
+
+    # Update FPS of Video stream
+    def update_fps(self):
+        now = time.time()
+        if str(self.start_time).split(".")[0] != str(now).split(".")[0]:
+            self.fps_show = self.fps
+        self.start_time = now
+        self.frame_time = now - self.frame_start_time
+        self.fps = 1.0 / self.frame_time
+        self.frame_start_time = now
+
+        self.label_fps["text"] = str(self.fps.__round__(2))
+    def get_frame(self):
+            try:
+                if self.cap.isOpened():
+                    ret, frame = self.cap.read()
+                    return ret, cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            except:
+                print("Error: No video input!!!")
+
+    # Main function for face detection and saving
+    def process(self):
+        ret, self.current_frame = self.get_frame()
+        faces = detector(self.current_frame, 0)
+        # Get frame
+        if ret:
+            self.update_fps()
+            self.label_face_in_frame["text"] = str(len(faces))
+            # Face detected
+            if len(faces) != 0:
+                # Show the ROI of faces
+                for k, d in enumerate(faces):
+                    self.face_ROI_width_start = d.left()
+                    self.face_ROI_height_start = d.top()
+                    # Compute the size of rectangle box
+                    self.face_ROI_height = (d.bottom() - d.top())
+                    self.face_ROI_width = (d.right() - d.left())
+                    self.hh = int(self.face_ROI_height / 2)
+                    self.ww = int(self.face_ROI_width / 2)
+
+                    # If the size of ROI > 480x640, output "out of range"
+                    if (d.right() + self.ww) > 640 or (d.bottom() + self.hh > 480) or (d.left() - self.ww < 0) or (
+                            d.top() - self.hh < 0):
+                        self.label_warning["text"] = "OUT OF RANGE"
+                        self.label_warning['fg'] = 'red'
+                        self.out_of_range_flag = True
+                        color_rectangle = (255, 0, 0)
+                    else:
+                        self.out_of_range_flag = False
+                        self.label_warning["text"] = ""
+                        color_rectangle = (255, 255, 255)
+                    self.current_frame = cv2.rectangle(self.current_frame,
+                                                       tuple([d.left() - self.ww, d.top() - self.hh]),
+                                                       tuple([d.right() + self.ww, d.bottom() + self.hh]),
+                                                       color_rectangle, 2)
+            self.current_frame_faces_cnt = len(faces)
+
+            # Convert PIL.Image.Image to PIL.Image.PhotoImage
+            img_Image = Image.fromarray(self.current_frame)
+            img_PhotoImage = ImageTk.PhotoImage(image=img_Image)
+            self.label.img_tk = img_PhotoImage
+            self.label.configure(image=img_PhotoImage)
+
+        # Refresh frame
+        self.window.after(20, self.process)
+
+    def run(self):
+        self.Folders()
+        self.check_existing_faces_cnt()
+        self.GUI_info()
+        self.process()
+        self.window.mainloop()
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+    Face_Register_con = Face_Register()
+    Face_Register_con.run()
+
+
+if __name__ == '__main__':
+    main()
