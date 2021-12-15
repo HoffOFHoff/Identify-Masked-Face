85c6c06067bf75ff4086317dc2606126d170b029
Bo Chao <91092213+pkocattoss@users.noreply.github.com>
2021-12-15 16:35:45 -0500

    update

--- FinalVersion.py
+++ FinalVersion.py
@@ -5,29 +5,29 @@ import logging
 def interface():
     print("If you want to take photos enter 'g' ")
     print("If you want to come to the Facial recognition Login enter 'l' ")
-    print("If you want to exit enter 'Q' ")
+    print("If you want to exit enter 'q' ")
     test = True
     # show the user interface 
     
     while test:
-        mood_type = input('Enter your mood:')
-        if mood_type == "1":
+        Instruction = input('Enter your instruction:')
+        if Instruction == "g":
             print("first step")
             from GUI import Face_Register
             logging.basicConfig(level=logging.INFO)
             Face_Register_con = Face_Register()
             Face_Register_con.run()
             from Mask_adding import mask_img
-            # mask_img()
+            mask_img()
             
-        if mood_type == "2":
+        if Instruction == "l":
             print("second step")
             from Samility_detect import image_show
             from worked import show_the_user
             # show_the_user()
             
             # image_show()
-        if mood_type == "q":
+        if Instruction == "q":
             test = False
             print("See you!That's the end.")
             os._exit(0)
