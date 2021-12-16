bfaaf756b4e979a872863729159c1538ce2f17ef
Bo Chao <91092213+pkocattoss@users.noreply.github.com>
2021-12-16 11:29:45 -0500

    Renaming

--- Interface.py
+++ Interface.py
@@ -5,18 +5,18 @@ Final_Project -Identify-Masked-Face-
 ************ Final Version.The combined version ***********
 
 In this project, we want to show a placid everyday scene -- you will stare at the 
-webcam, and here is a line lettering: Who you are. Our project could recognize who you
-are even when you wear mask! The most exciting thing is that: we do not need to use
-model or machine learning method to make it.
+webcam, and it doesn't recognize you because you are wearing a mask. Our project could recognize who you
+are even when you wear mask!
 It won't take up a lot of your computer's memory to train complex models! We only need 
-the package self-contained tools in python: Face_recognition. Then we use artificial 
-masked face as the new model.
-Is it cool?
+the packages which are self-contained tools in python: Face_recognition. Then we use artificial 
+masked faces as the new model.
+Isn't that cool?
 Now let's enjoy it!
 '''
-# Firstly, we need to import some useful packge for our project.
-# Since we seperate our 700 around lines into four documents, some package will
-# show in the other place. 
+# Firstly, we need to import some useful packges for our project.
+# Our project contains five parts. The Interface as the main document to run the programe,
+# GUI takes all photos and store them, Mask_adding puts on virtual masks for recorded faces,
+# Similarity_detect compares the images to present its similarity, worked identifies your face with the camera. 
 # The merged version of the face recognition project
 import os
 import logging
@@ -41,24 +41,24 @@ def interface():
         One folder Masked_faces
 
     '''
-    print("If you want to take photos enter 'g' ")
-    print("If you want to come to the Facial recognition Login enter 'l' ")
-    print("If you want to exit enter 'q' ")
+    print("1. Please take a photo of your self, enter 'g' ")
+    print("2. Appluy facial recognition, enter 'l' ")
+    print("If you want to exit, enter 'q' ")
     test = True
     # show the user interface 
     while test:
         Instruction = input('Enter your instruction:')
         if Instruction == "g":
-            print("first step")
+            print("Step 1")
             print("Now please take off your mask")
-            print("We recommend you inpput your name such as 'Jack' ")
-            print("You should not use '_' or '/' ")
+            print("We recommend you input your name in the format of 'Jack' ")
+            print("Please avoid using '_' or '/' ")
             print("///////////////////////////////")
             from GUI import Face_Register
             logging.basicConfig(level=logging.INFO)
             Face_Register_con = Face_Register()
             Face_Register_con.run()
-            print("Now you will see your artificial masked face")
+            print("Now you will see your face with an artificial mask")
             print("///////////////////////////////")       
             from Mask_adding import mask_img,find_landmasks
             facelandmarks = find_landmasks()
@@ -66,27 +66,28 @@ def interface():
             ori_img_dir=os.path.abspath(list_of_user[0])
             mask_img(ori_img_dir, mask_img_dir="images/surgical.png",
              facelandmarks=facelandmarks)
-            print("Now you could type l and come to second setp")
+            print("Please type l to perfrom step 2")
             print("///////////////////////////////") 
             
         if Instruction == "l":
-            print("second step")
-            print("Now you could take on your mask and take new photo")
-            print("We recommend you inpput a new name such as 'JackMASKED' ")
-            print("You should not use '_' or '/' ")
+            print("Step 2")
+            print("Do not press the clear botton in the GUI!!!!")
+            print("Please put on your mask and take a new photo")
+            print("We recommend you input a new name in the format of 'JackMASKED' ")
+            print("Please avoid using '_' or '/' ")
             print("///////////////////////////////") 
             from GUI import Face_Register
             logging.basicConfig(level=logging.INFO)
             Face_Register_con = Face_Register()
             Face_Register_con.run()
-            print("Now you could see the comporison.png show our result")
+            print("Now you can see the comporison.png showing the comparison result")
             print("///////////////////////////////") 
             from Similarity_detect import img_show
             img_show()
-            print("Now you could see the video webcam interface")
-            print("You could find your name is showed under your face")
-            print("When you wear mask, it will show name like  JackMASKED ,")
-            print("When you do not wear anything, it will show the first name you type in")
+            print("Now you can see the video webcam interface")
+            print("You can find your name showen under the red boxs")
+            print("When you wear a mask, the user name you input while wearing a mask will be presented,")
+            print("When you present your face without a mask, it will show the first name you typed in")
             print("///////////////////////////////") 
             from worked import show_the_user
             show_the_user()
