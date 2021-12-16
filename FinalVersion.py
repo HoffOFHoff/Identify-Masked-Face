9e71a382112c438b007ea7ff778325e2a239082e
HoffOFHoff <87690274+HoffOFHoff@users.noreply.github.com>
2021-12-16 11:04:16 -0500

    update

--- FinalVersion.py
+++ FinalVersion.py
@@ -1,44 +1,96 @@
+# -*- coding: utf-8 -*-
+'''
+EN.640.635 Software Carpentry
+Final_Project -Identify-Masked-Face-
+************ Final Version.The combined version ***********
+
+In this project, we want to show a placid everyday scene -- you will stare at the 
+webcam, and here is a line lettering: Who you are. Our project could recognize who you
+are even when you wear mask! The most exciting thing is that: we do not need to use
+model or machine learning method to make it.
+It won't take up a lot of your computer's memory to train complex models! We only need 
+the package self-contained tools in python: Face_recognition. Then we use artificial 
+masked face as the new model.
+Is it cool?
+Now let's enjoy it!
+'''
+# Firstly, we need to import some useful packge for our project.
+# Since we seperate our 700 around lines into four documents, some package will
+# show in the other place. 
 # The merged version of the face recognition project
 import os
 import logging
 import glob
 from PIL import Image
-
+# The main body of our project
 def interface():
+    '''
+    Combine part1.GUI, part2.Masked_adding, part3.Similarity_detect and
+    part4.worked together, so it could save time and make the whole project
+    more readable and visible.
+
+    **Parameters**
+        fptr: *str*
+            The
+        mask: *int*
+            The 
+
+    **Returns**
+        One comparison.png
+        One folder data/faces_from_camera
+        One folder Masked_faces
+
+    '''
     print("If you want to take photos enter 'g' ")
     print("If you want to come to the Facial recognition Login enter 'l' ")
     print("If you want to exit enter 'q' ")
     test = True
     # show the user interface 
-
     while test:
         Instruction = input('Enter your instruction:')
         if Instruction == "g":
             print("first step")
+            print("Now please take off your mask")
+            print("We recommend you inpput your name such as 'Jack' ")
+            print("You should not use '_' or '/' ")
+            print("///////////////////////////////")
             from GUI import Face_Register
             logging.basicConfig(level=logging.INFO)
             Face_Register_con = Face_Register()
             Face_Register_con.run()
-            
+            print("Now you will see your artificial masked face")
+            print("///////////////////////////////")       
             from Mask_adding import mask_img,find_landmasks
             facelandmarks = find_landmasks()
             list_of_user = glob.glob(r"data/faces_from_camera/*/*.png")
             ori_img_dir=os.path.abspath(list_of_user[0])
             mask_img(ori_img_dir, mask_img_dir="images/surgical.png",
              facelandmarks=facelandmarks)
+            print("Now you could type l and come to second setp")
+            print("///////////////////////////////") 
             
         if Instruction == "l":
             print("second step")
+            print("Now you could take on your mask and take new photo")
+            print("We recommend you inpput a new name such as 'JackMASKED' ")
+            print("You should not use '_' or '/' ")
+            print("///////////////////////////////") 
             from GUI import Face_Register
             logging.basicConfig(level=logging.INFO)
             Face_Register_con = Face_Register()
             Face_Register_con.run()
-            from Samility_detect import img_show
+            print("Now you could see the comporison.png show our result")
+            print("///////////////////////////////") 
+            from Similarity_detect import img_show
             img_show()
+            print("Now you could see the video webcam interface")
+            print("You could find your name is showed under your face")
+            print("When you wear mask, it will show name like  JackMASKED ,")
+            print("When you do not wear anything, it will show the first name you type in")
+            print("///////////////////////////////") 
             from worked import show_the_user
             show_the_user()
-            
-            # image_show()
+        # Whenever you want leave, you could type q
         if Instruction == "q":
             test = False
             print("See you!That's the end.")
---
+++ Similarity_detect.py
@@ -0,0 +1,141 @@
+# -*- coding: utf-8 -*-
+'''
+EN.640.635 Software Carpentry
+Final_Project -The Similarity_detect
+
+Part 3.Similarity comparison 
+
+In this project, we want to write one function that can compare the 
+similarity between our artificial masked artificial face with the masked face
+in our life. Then we will show the SSIM parameter as the indicator of evaluation.
+The img_show() function will be used in the mainbody--FinalVersion.py and the picture
+will be saved in our Identify-Masked-Face project. 
+You could see that the faked masked face is close to the real masked face, and
+is closer in difference.
+1. We generate the interface of the img_show comparison.
+2. Use skimage to detect the MSE and SSIM parameter to check the difference
+3. The comparison will show if it think that those artificial mask are the 
+sanme with the real masked face in daily life.
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+import os
+import glob
+from skimage.metrics import structural_similarity as ssim
+from skimage.metrics import mean_squared_error
+from skimage import transform,data
+from skimage import io
+from PIL import Image
+
+def img_show():
+    '''
+    Apply skimage to determine the similarity of the line structure of an image. 
+    It needs both the original image and your newly-masked image and . 
+    There will be a figure show to compare your face with
+    our model. In the meanwhile, there will be one figure show the detailed 
+    comparison which saved in-situ.
+
+    **Parameters**
+
+        list_of_user: *list*
+            All the people once taked photo with our project.
+            (ex.['/Users/xuziying/Desktop/data/faces_from_camera/person_1
+            _chaobo/img_face_1.png','/Users/xuziying/Desktop/data/faces_
+            from_camera/person_2_ziying/img_face_1.png'])
+
+        img_path: *str*
+            This varible store the absolute path of our photo from webcam. 
+            (ex. /Users/xuziying/Desktop/data/faces_from_camera/person_1_chaobo).
+        
+        img_mask_path: *str*
+            This varible store the absolute path of the photo which we add an 
+            artificial mask on it. 
+            (ex. /Users/xuziying/Desktop/data/Masked_faces/person_1_chaobo_surgical.png).        
+        
+        ssim_masked: *turple*
+            The complex model to determine the similarity between the artificial masked
+            picture and the real face without mask.
+
+        ssim_real: *turple*
+            The complex model to determine the similarity between the real face with
+            mask and the artificial face with mask.
+
+    **Returns**
+
+        None
+        
+
+    For extra info:
+
+        *https://scikit-image.org/
+    '''
+
+    # We use three images seperately, and compare those figures.
+    # Here we convert to RGB to circumvent some issues with different
+    # file formats.
+    list_of_user = glob.glob(r"data/faces_from_camera/*/*.png")
+    img_path = list_of_user[0]
+    img_real_path = list_of_user[1]
+    print(img_path)
+    print(img_real_path)
+    list_of_artificial_user = glob.glob(r"data/Masked_faces/*.png")
+    img_mask_path = list_of_artificial_user[0]
+    print(img_mask_path)
+
+    # Usually, when we add mask to our face, the size of the picture will
+    # change, so we use the transform.resize function to make they casually
+    # similar. Otherwise the comparison will fail.
+    img = io.imread(img_path)
+    (x,y,z)=img.shape
+    img_mask = io.imread(img_mask_path)
+    img_mask = transform.resize(img_mask,(x,y,z))
+    img_real = io.imread(img_real_path)
+    img_real = transform.resize(img_real,(x,y,z))
+
+    # Generate the interface of the img_show comparison.
+    fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(10, 4),
+                            sharex=True, sharey=True)
+    ax = axes.ravel()
+
+    mse_none = mean_squared_error(img, img)
+    ssim_none = ssim(img, img, multichannel=True, data_range=img.max() - img.min())
+
+    mse_masked = mean_squared_error(img, img_mask)
+    ssim_masked = ssim(img, img_mask, multichannel=True,
+                    data_range=img_mask.max() - img_mask.min())
+
+    mse_real = mean_squared_error(img, img_real)
+    ssim_real = ssim(img_mask, img_real, multichannel=True,
+                    data_range=img_real.max() - img_real.min())
+
+    # Use the model of MSE and SSIM to compare the similarity.
+    # SSIM means structural similarity index measure.
+    # MSE means Mean Squared Error.
+    # When SSIM([0,1]) is close to 0, the two fig are similar and vice versa.
+    # WHen MSE is close to 0, the two fig are similar and vice versa.
+    ax[0].imshow(img, cmap=plt.cm.gray, vmin=0, vmax=1)
+    ax[0].set_xlabel(f'MSE: {mse_none:.2f}, SSIM: {ssim_none:.2f}')
+    ax[0].set_title('Original image')
+
+    ax[1].imshow(img_mask, cmap=plt.cm.gray, vmin=0, vmax=1)
+    ax[1].set_xlabel(f'MSE: {mse_masked:.2f}, SSIM: {ssim_masked:.2f}')
+    ax[1].set_title('Image with mask')
+
+    ax[2].imshow(img_real, cmap=plt.cm.gray, vmin=0, vmax=1)
+    ax[2].set_xlabel(f'MSE: {mse_real:.2f}, SSIM: {ssim_real:.2f}')
+    ax[2].set_title('Image in real life')
+
+    # The comparison will show 
+    if  ssim_real > 0.5:
+        print('the same person')
+    else:
+        print('a bit different')
+
+
+    plt.tight_layout()
+    plt.savefig('comparison.png',bbox_inches='tight')
+    plt.show()
+
+img_show()
